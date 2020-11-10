bb871b9ac488ddb22dbb1c2ccb2069591628fac7
Taejin-Mac <taejin7824@gmail.com>
2020-11-10 17:29:16 +0900

    Change Indentation into 4

--- script/function/cv2Init.py
+++ script/function/cv2Init.py
@@ -1,10 +1,11 @@
 import cv2 
 import numpy as np
-#빈 캔버스 만들기
+
+# 빈 캔버스 만들기
 def init() : 
-  blank = np.zeros([512,512],dtype=np.uint8)
-  blank.fill(255)
-  blank = cv2.resize(blank,(512,512))
+    blank = np.zeros([512,512],dtype=np.uint8)
+    blank.fill(255)
+    blank = cv2.resize(blank,(512,512))
   
-  return blank
+    return blank
   
--- script/function/map2Image.py
+++ script/function/map2Image.py
@@ -3,19 +3,18 @@ import os
 import pandas as pd
 
 def map2Image(minX,minY,maxX,maxY,csv_file):
+    inputImage = np.zeros([512,512], dtype=np.uint8)
 
-  inputImage = np.zeros([512,512], dtype=np.uint8)
+    for i in range(0,csv_file.shape[0]):
+        x = csv_file.loc[i][0]
+        y = csv_file.loc[i][1]
 
-  for i in range(0,csv_file.shape[0]):
-    x = csv_file.loc[i][0]
-    y = csv_file.loc[i][1]
+        mapX = int(round(np.interp(x,[minX,maxX],[0,500])))
+        mapY = int(round(np.interp(y,[minY,maxY],[0,500])))
+        inputImage[mapX][mapY] = 1
 
-    mapX = int(round(np.interp(x,[minX,maxX],[0,500])))
-    mapY = int(round(np.interp(y,[minY,maxY],[0,500])))
-    inputImage[mapX][mapY] = 1
+    outputImage = drawNp(inputImage)
 
-  outputImage = drawNp(inputImage)
+    rotImage = np.rot90(outputImage)
 
-  rotImage = np.rot90(outputImage)
-
-  return rotImage
+    return rotImage
